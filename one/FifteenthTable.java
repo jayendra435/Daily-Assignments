80e738443f0ab1550514b303061266e16b2cb8bf
jayendra435 <s170435@rguktsklm.ac.in>
2023-09-13 10:27:30 +0530

    first commit

---
+++ one/FifteenthTable.java
@@ -0,0 +1,14 @@
+package one;
+
+public class FifteenthTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			System.out.printf("%d x %d = %d\n",15,i,15*i);
+		}
+		
+
+	}
+
+}
